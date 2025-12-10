80402a2c0f41503d415eb1ca59e6c77a40885dcd
Zerive05 <bgsman05@gmail.com>
2025-12-10 20:37:52 +0700

    after presentation

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Ganti IP ini jika IP Laptop berubah
-  static const String baseUrl = "http://192.168.1.19/api_mispet";
+  static const String baseUrl = "http://192.168.1.13/api_mispet";
 
   static const String register = "$baseUrl/register.php";
   static const String login = "$baseUrl/login.php";
